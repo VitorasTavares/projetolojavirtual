2904bd4f8dacfef4a1170bdcae21a6496a21be33
VitorasTavares <vitorastavares@gmail.com>
2019-09-30 11:57:09 -0300

    adicionando fluxo login

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
-    $nomeSistema = "Cursos do Vitor";
+    $nomeSistema = "Cursos Vitor";
+    //$usuario = ["nome"=>"Vitor"];
 ?>
 
 <!DOCTYPE html>
@@ -18,42 +19,51 @@
     </h1>
         <nav>
             <ul class="nav">
+            <?php if(isset($usuario) && $usuario !="" ) {?>
                 <li class="nav-item">
                     <a class="nav-link" href="#">Curso</a>
                 </li>
+                <li class="nav-item"> 
+                    <a class="nav-link" href="#">Olá <?php echo $usuario['nome']; ?></a>
+                </li>
+            <?php } else { ?>
                 <li class="nav-item"> 
                     <a class="nav-link" href="#">Login</a>
                 </li>
                 <li class ="nav-item">
                     <a class="nav-link" href="#">Cadastrar</a>
                 </li>
+            <?php } ?>
             </ul>       
         </nav>
 </header>
 <main>
     <section class="container">
             <div class="row justify-content-around">
-                <div class="col-lg-3-card text-center">
-                    <h2>Titulo Produto</h2>
+                <div class="col-lg-3 card text-center">
+                    <h2> Produto</h2>
                       <img src="img/imagem1.jpg" class="card-img-top" alt="...">
                         <div class="card-body">
                         <h5 class="card-title">R$15,00</h5>
                         <a href="#" class="btn btn-primary">Comprar</a>
                  </div>
-                 <div class="col-lg-3-card text-center">
-                    <h2>Titulo Produto</h2>
+            </div> 
+                 <div class="col-lg-3 card text-center">
+                    <h2> Produto</h2>
                       <img src="img/imagem1.jpg" class="card-img-top" alt="...">
                         <div class="card-body">
-                        <h5 class="card-title">R$15,00</h5>
-                        <a href="#" class="btn btn-primary">Comprar</a>
+                            <h5 class="card-title">R$15,00</h5>
+                            <a href="#" class="btn btn-primary">Comprar</a>
+                       </div>
                  </div>
-                 <div class="col-lg-3-card text-center">
-                    <h2>Titulo Produto</h2>
+                 <div class="col-lg-3 card text-center">
+                    <h2> Produto</h2>
                       <img src="img/imagem1.jpg" class="card-img-top" alt="...">
                         <div class="card-body">
-                        <h5 class="card-title">R$15,00</h5>
-                        <a href="#" class="btn btn-primary">Comprar</a>
-                 </div>
+                             <h5 class="card-title">R$15,00</h5>
+                            <a href="#" class="btn btn-primary">Comprar</a>
+                        </div>
+                </div>
             </div>
    </section>
   
